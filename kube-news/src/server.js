38b4394931ab55b7ac427e22afe295982d890ba6
Fabricio Veronez <fabricioveronez@gmail.com>
2023-10-11 16:57:25 -0300

    Commit Inicial

---
+++ kube-news/src/server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+const models = require('./models/post')
+const bodyParser = require('body-parser')
+const promBundle = require("express-prom-bundle");
+const config = require('./system-life');
+const middlewares = require('./middleware')
+
+const metricsMiddleware = promBundle({
+    includeMethod: true, 
+    includePath: true, 
+    includeStatusCode: true, 
+    includeUp: true,
+    promClient: {
+        collectDefaultMetrics: {
+        }
+      }
+});
+
+app.use(middlewares.countRequests)
+app.use(metricsMiddleware)
+app.use(config.middlewares.healthMid);
+app.use('/', config.routers);
+app.use(express.static('static'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs');
+
+
+app.get('/post', (req, res) => {
+    res.render('edit-news', {post: {title: "", content: "", summary: ""}});
+});
+
+app.post('/post', (req, res) => {
+
+    let valid = true;
+
+    if ((req.body.title.length !== 0 && req.body.title.length < 30) && 
+        (req.body.resumo.length !== 0 && req.body.resumo.length < 50) &&
+        (req.body.description.length !== 0 && req.body.description.length < 2000)) {
+        valid = true;
+    } else {
+        valid = false;
+    }
+
+    if (valid) {
+        models.Post.create({title: req.body.title, content: req.body.description, summary: req.body.resumo, publishDate: Date.now()});
+        res.redirect('/');
+    } else {
+        res.render('edit-news', {post: {title: req.body.title, content: req.body.description, summary: req.body.resumo}, valido: false});
+    }
+    
+});
+
+app.post('/api/post', (req, res) => {
+    models.Post.create({title: req.body.title, content: req.body.description, summary: req.body.resumo, publishDate: Date.now()});
+    res.write("ok")
+});
+
+app.get('/post/:id', async (req, res) => {
+
+    const post = await models.Post.findByPk(req.params.id);
+    res.render('view-news',{post: post});
+});
+
+
+app.get('/', async (req, res) => {
+
+    const posts = await models.Post.findAll();
+    res.render('index',{posts: posts});
+});
+
+models.initDatabase();
+app.listen(8080);
+
+console.log('Aplicação rodando na porta 8080');
